3d2818416551bf41c2390bfa27907948455b0b89
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2017-09-21 13:25:50 +0300

    Create createtable.php

---
+++ createtable.php
@@ -0,0 +1,9 @@
+Server=tcp:pinyasova.database.windows.net,1433;
+Initial Catalog=Прогр;
+Persist Security Info=False;
+User ID={your_username};
+Password={your_password};
+MultipleActiveResultSets=False;
+Encrypt=True;
+TrustServerCertificate=False;
+Connection Timeout=30;
