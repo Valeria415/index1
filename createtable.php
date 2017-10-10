3a4c63885f5d0ef7cf561b4237c8336295cd385f
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2017-10-10 10:12:44 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -1,6 +1,6 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Прогр", "Valera", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Прогр", "Valera", "Hswfhmlyz08");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -9,6 +9,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Valera@pinyasova", "pwd" => "{hswfhmlyz08}", "Database" => "Прогр", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "Valera@pinyasova", "pwd" => "Hswfhmlyz08", "Database" => "Прогр", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:pinyasova.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
