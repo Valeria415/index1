6d8bbc34b12799f6a6f3c7b9d1342df6a308aec4
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2017-10-10 10:16:42 +0300

    Update createtable.php

--- createtable.php
+++ createtable.php
@@ -15,7 +15,3 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "Valera@pinyasova", "pwd" => "Hswfhmlyz08", "Database" => "Прогр", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:pinyasova.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
