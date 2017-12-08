d7dd59a4d2440478543ba1792dae8d095658dcf9
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2017-12-08 15:14:14 +0400

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ $password = $_POST['password'];
     
 // Insert data
 $sql_insert =
-"INSERT INTO test_tbl1 (Имя, email, date, password) VALUES (?,?,?,?)";
+"INSERT INTO test_tbl1 (name, email, date, password) VALUES (?,?,?,?)";
 $stmt = $conn->prepare($sql_insert);
 $stmt->bindValue(1, $name);
 $stmt->bindValue(2, $email);
@@ -73,13 +73,13 @@ $stmt->execute();
 catch(Exception $e) {
 die(var_dump($e));
 }
-echo "<h3>Your're registered!</h3>";
+echo "<h3>Вы зарегестированы!</h3>";
 }
 $sql_select = "SELECT * FROM test_tbl1";
 $stmt = $conn->query($sql_select);
 $registrants = $stmt->fetchAll();
 if(count($registrants) > 0) {
-echo "<h2>People who are registered:</h2>";
+echo "<h2>Люди, которые зарегестрированы:</h2>";
 echo "<table>";
 echo "<tr><th>Name</th>";
 echo "<th>Email</th>";
@@ -93,7 +93,7 @@ echo "<td>".$registrant['password']."</td></tr>";
 }
 echo "</table>";
 } else {
-echo "<h3>No one is currently registered.</h3>";
+echo "<h3>В настоящее время никто не зарегистрирован.</h3>";
 }  
  
     
@@ -101,9 +101,9 @@ $password = $_POST['password'];
 $confirmpassword = $_POST['confirm_password'];
     if($password == $confirmpassword)
     {
-        echo "<h3>пароль введен верно</h3>";
+        echo "<h3>Пароль введен верно</h3>";
     }
-    else {echo "<h3>пароль введен неверно</h3>";}
+    else {echo "<h3>Пароль введен неверно</h3>";}
 
 ?>
    
