edb1220c258101c6efe0ce72858528f0bd92daff
Valeria415 <31986489+Valeria415@users.noreply.github.com>
2017-12-19 10:28:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -120,20 +120,19 @@ if(count($registrants) > 0) {
     echo "<h3>Ни один пользователь не зарегистрирован.</h3>";
 }
 
-if(isset($_POST["submit3"]))
+/* if(isset($_POST["submit3"]))
 {
+    try {
+$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "Valera", "Hswfhmlyz08");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 $sql1 = "DELETE FROM test_tbl1";
 $conn->query($sql1);
-}
 }
 catch (PDOException $e) {
 print("Error connecting to SQL Server.");
 die(print_r($e));
 }
-try {
-$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "Valera", "Hswfhmlyz08");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-}
+*/
 ?>
    
 </form>
